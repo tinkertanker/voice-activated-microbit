52269d79385723b365a23bd8d6144bde59452f6c
Mireille-T <69097387+Mireille-T@users.noreply.github.com>
2020-12-31 14:08:52 +0800

    Updated file to final version referenced in slides

--- source/MicrophoneInferenceTest.cpp
+++ source/MicrophoneInferenceTest.cpp
@@ -29,13 +29,17 @@ DEALINGS IN THE SOFTWARE.
 #include "edge-impulse-sdk/classifier/ei_run_classifier.h"
 #include "edge-impulse-sdk/dsp/numpy.hpp"
 
-#define INFERENCING_KEYWORD     "wow" // change according to keyword selected; originally "microbit"
+#define INFERENCING_KEYWORD     "microbit"
+#define ANOTHER_KEYWORD         "house"
 
 static NRF52ADCChannel *mic = NULL;
 static ContinuousAudioStreamer *streamer = NULL;
 static StreamNormalizer *processor = NULL;
 
 static inference_t inference;
+bool lightIsOn_0 = false; // records if LED at Pin 0 is on or off
+bool lightIsOn_1 = false; // records if LED at Pin 1 is on or off
+int count = 0; // ensures that the light switching on happens only once per keyword
 
 /**
  * Get raw audio signal data
@@ -58,6 +62,36 @@ static void heard_keyword() {
         000,000,000,000,000\n";
     MicroBitImage img(happy_emoji);
     uBit.display.print(img);
+    ++count; // count increases when keyword is heard 
+    if (lightIsOn_0 && count < 2){ // if light is on and not already turning off
+        uBit.io.P0.setDigitalValue(0); // switch off LED at Pin 0
+        lightIsOn_0 = false; // LED is now off
+    }
+    else if (count < 2){ // if light is off and not already turning on
+        uBit.io.P0.setDigitalValue(1); // switch on LED at Pin 0
+        lightIsOn_0 = true; // LED is now on
+    }
+}
+
+// Invoked when we hear another keyword
+static void heard_another_keyword() {
+    const char * empty_emoji ="\
+        000,000,255,000,000\n\
+        000,255,255,255,000\n\
+        255,255,255,255,255\n\
+        000,255,255,255,000\n\
+        000,255,000,255,000\n";
+    MicroBitImage img(empty_emoji);
+    uBit.display.print(img);
+    ++count; // count increases when keyword is heard 
+    if (lightIsOn_1 && count < 2){ // if light is on and not already turning off
+        uBit.io.P1.setDigitalValue(0); // switch off LED at Pin 1
+        lightIsOn_1 = false; // LED is now off
+    }
+    else if (count < 2){ // if light is off and not already turning on
+        uBit.io.P1.setDigitalValue(1); // switch on LED at Pin 1
+        lightIsOn_1 = true; // LED is now on
+    }
 }
 
 /**
@@ -72,7 +106,20 @@ static void heard_other() {
         000,000,000,000,000\n";
     MicroBitImage img(empty_emoji);
     uBit.display.print(img);
+    count = 0; // resets count
 }
+
+ // Invoked when something happens
+ static void debug() {
+     const char * debug_emoji ="\
+        000,000,000,000,128\n\
+        000,000,000,000,000\n\
+        000,000,255,000,000\n\
+        000,000,000,000,000\n\
+        255,000,000,000,000\n";
+    MicroBitImage img(debug_emoji);
+    uBit.display.print(img);
+ }
 
 void
 mic_inference_test()
@@ -121,8 +168,12 @@ mic_inference_test()
 
     // number of frames since we heard 'microbit'
     uint8_t last_keywords = 0b0;
+    uint8_t last_another_keywords = 0b0;
 
     int heard_keyword_x_ago = 100;
+    int heard_another_keyword_x_ago = 100;
+    uBit.io.P0.setDigitalValue(0); // switches off LED at Pin 0 when code first runs
+    uBit.io.P1.setDigitalValue(0); // switches off LED at Pin 1 when code first runs
 
     while(1) {
         uBit.sleep(1);
@@ -144,34 +195,55 @@ mic_inference_test()
             }
 
             bool heard_keyword_this_window = false;
+            bool heard_another_keyword_this_window = false;
 
             if (++print_results >= 0) {
                 // print the predictions
                 ei_printf("Predictions (DSP: %d ms., Classification: %d ms.): \n",
                     result.timing.dsp, result.timing.classification);
                 for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
-                    ei_printf("    %s: ", result.classification[ix].label);
+                    ei_printf("%s: ", result.classification[ix].label);
                     ei_printf_float(result.classification[ix].value);
                     ei_printf("\n");
 
-                    if (strcmp(result.classification[ix].label, INFERENCING_KEYWORD) == 0 && result.classification[ix].value > 0.7) {
+                    if (strcmp(result.classification[ix].label, INFERENCING_KEYWORD) == 0 && result.classification[ix].value > 0.4) {
                         heard_keyword_this_window = true;
                     }
+                    
+                    else if (strcmp(result.classification[ix].label, ANOTHER_KEYWORD) == 0 && result.classification[ix].value > 0.4) {
+                        heard_another_keyword_this_window = true;
+                    }
+                    
                 }
 
                 last_keywords = last_keywords << 1 & 0x1f;
+                last_another_keywords = last_another_keywords << 1 & 0x1f;
 
                 if (heard_keyword_this_window) {
                     last_keywords += 1;
                 }
+                else if (heard_another_keyword_this_window) {
+                    last_another_keywords += 1;
+                }
 
                 uint8_t keyword_count = 0;
+                uint8_t another_keyword_count = 0;
                 for (size_t ix = 0; ix < 5; ix++) {
                     keyword_count += (last_keywords >> ix) & 0x1;
+                }
+                for (size_t ix = 0; ix < 5; ix++) {
+                    another_keyword_count += (last_another_keywords >> ix) & 0x1;
                 }
 
                 if (heard_keyword_this_window) {
                     ei_printf("\nHeard keyword: %s (%d times, needs 5)\n", INFERENCING_KEYWORD, keyword_count);
+                    last_keywords = 0;
+                    heard_keyword_x_ago = 0;
+                }
+                else if (heard_another_keyword_this_window) {
+                    ei_printf("\nHeard keyword: %s (%d times, needs 5)\n", ANOTHER_KEYWORD, another_keyword_count);
+                    last_another_keywords = 0;
+                    heard_another_keyword_x_ago = 0;
                 }
 
                 if (keyword_count >= 1) {
@@ -179,12 +251,21 @@ mic_inference_test()
                     last_keywords = 0;
                     heard_keyword_x_ago = 0;
                 }
+                else if (another_keyword_count >= 1) {
+                    ei_printf("\n\n\nDefinitely heard keyword: \u001b[32m%s\u001b[0m\n\n\n", ANOTHER_KEYWORD);
+                    last_another_keywords = 0;
+                    heard_another_keyword_x_ago = 0;
+                }
                 else {
                     heard_keyword_x_ago++;
+                    heard_another_keyword_x_ago++;
                 }
 
                 if (heard_keyword_x_ago <= 4) {
                     heard_keyword();
+                }
+                else if (heard_another_keyword_x_ago <= 4) {
+                    heard_another_keyword();
                 }
                 else {
                     heard_other();
